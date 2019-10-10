1d5c678a65ca7e389fee22a4d132bc4814cadec5
Wincent Balin <wincent.balin@gmail.com>
2019-10-11 01:16:22 +0200

    Added module setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pytesstrain',
+    version='0.1.0',
+    packages=['pytesstrain'],
+    url='https://github.com/wincentbalin/pytesstrain',
+    license='Apache License (2.0)',
+    author='Wincent Balin',
+    author_email='wincent.balin@gmail.com',
+    description='Collection of utilities for Tesseract OCR training',
+    install_requires=['pytesseract', 'jiwer']
+)
