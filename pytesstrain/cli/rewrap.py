b2bfb5a60ce566f472629787ad801e774f21f8a4
Wincent Balin <wincent.balin@gmail.com>
2019-10-11 01:16:44 +0200

    Added rewrap utility

---
+++ pytesstrain/cli/rewrap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Rewrap lines without breaking words"""
+
+import sys
+import argparse
+import textwrap
+
+
+def main():
+    parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
+    parser.add_argument('infile', type=argparse.FileType('r', encoding='UTF-8'), help='Input file')
+    parser.add_argument('outfile', type=argparse.FileType('w', encoding='UTF-8'), help='Output file')
+    parser.add_argument('width', type=int, help='Output line width')
+    args = parser.parse_args()
+
+    for line in args.infile:
+        for wrapped in textwrap.wrap(line, args.width):
+            args.outfile.write('{}\n'.format(wrapped))
+
+
+if __name__ == '__main__':
+    main()
