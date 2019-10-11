2667e990e864652dbdf8df461fd58e1d8de444d9
Wincent Balin <wincent.balin@gmail.com>
2019-10-11 15:25:47 +0200

    Added text2image package
    This is a basically a copy of tesseract package from pytesseract. Some party from that packaged are used here too.

---
+++ pytesstrain/text2image/__init__.py
@@ -0,0 +1,1 @@
+from .pytext2image import *
---
+++ pytesstrain/text2image/pytext2image.py
@@ -0,0 +1,57 @@
+"""
+This file is essentially a reimplementation of pytesseract, adjusted for text2image.
+"""
+
+import sys
+import shlex
+import subprocess
+
+from pytesseract.pytesseract import subprocess_args, timeout_manager, get_errors, TesseractError
+
+
+# CHANGE THIS IF TEXT2IMAGE IS NOT IN YOUR PATH, OR IS NAMED DIFFERENTLY
+text2image_cmd = 'text2image'
+
+
+class Text2imageError(TesseractError):
+    pass
+
+
+class Text2imageNotFoundError(EnvironmentError):
+    def __init__(self):
+        super(Text2imageNotFoundError, self).__init__(
+            text2image_cmd + " is not installed or it's not in your path"
+        )
+
+
+def run_text2image(input_filename,
+                   output_filename_base,
+                   extension,
+                   lang,
+                   config='',
+                   nice=0,
+                   timeout=0):
+    cmd_args = []
+
+    if not sys.platform.startswith('win32') and nice != 0:
+        cmd_args += ('nice', '-n', str(nice))
+
+    cmd_args += (text2image_cmd, input_filename, output_filename_base)
+
+    if lang is not None:
+        cmd_args += ('-l', lang)
+
+    if config:
+        cmd_args += shlex.split(config)
+
+    if extension and extension not in {'box', 'osd', 'tsv'}:
+        cmd_args.append(extension)
+
+    try:
+        proc = subprocess.Popen(cmd_args, **subprocess_args())
+    except OSError:
+        raise Text2imageNotFoundError()
+
+    with timeout_manager(proc, timeout) as error_string:
+        if proc.returncode:
+            raise Text2imageError(proc.returncode, get_errors(error_string))
