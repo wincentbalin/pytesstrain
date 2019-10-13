d0832db4682faee6df3897f8dc858639c9ccdd5a
Wincent Balin <wincent.balin@gmail.com>
2019-10-14 00:17:48 +0200

    Fixed module path
    Previously, the module was accessible as pytesstrain.pytesstrain.*. Now it is correct path pytesstrain.*. It is still interesting what include parameter does here.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 setup(
     name='pytesstrain',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=['pytesstrain']),
     url='https://github.com/wincentbalin/pytesstrain',
     license='Apache License (2.0)',
     author='Wincent Balin',
