e3aeece290462d08d2df77e12a279319f42a695d
Wincent Balin <wincent.balin@gmail.com>
2019-10-15 22:35:18 +0200

    Added imports for test function

---
+++ pytesstrain/train/run_test.py
@@ -0,0 +1,13 @@
+"""
+Here are all test runners.
+"""
+
+from ..tesseract import run_and_get_output as run_tesseract
+from pytesseract.pytesseract import tesseract_cmd
+from ..text2image import run_and_get_output as run_text2image
+from ..text2image.pytext2image import text2image_cmd
+from ..utils import ChDir
+
+
+def run_test():
+    pass
