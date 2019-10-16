3798c1249c39a8dfa7e9594133128fe4ac596472
Wincent Balin <wincent.balin@gmail.com>
2019-10-16 02:28:52 +0200

    Refactored from dict to defaultdict

--- pytesstrain/ambigs/properties.py
+++ pytesstrain/ambigs/properties.py
@@ -3,20 +3,18 @@ Ambiguities properties.
 """
 
 import json
+from collections import defaultdict
 
 
 class AmbiguityProperties:
     def __init__(self, mandatory: bool):
         self.mandatory = mandatory
         self.count = 0
-        self.distribution = {}
+        self.distribution = defaultdict(int)
 
     def add(self, value):
         self.count += 1
-        if value in self.distribution:
-            self.distribution[value] += 1
-        else:
-            self.distribution[value] = 1
+        self.distribution[value] += 1
 
 
 class AmbiguityPropertiesJSONEncoder(json.JSONEncoder):
