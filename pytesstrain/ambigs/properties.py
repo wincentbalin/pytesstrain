d4e73c250a2127e5d079f33890dff16d47bbc9a3
Wincent Balin <wincent.balin@gmail.com>
2019-10-16 02:51:43 +0200

    Added function extract_ambiguities

--- pytesstrain/ambigs/properties.py
+++ pytesstrain/ambigs/properties.py
@@ -6,6 +6,9 @@ import json
 from collections import defaultdict
 
 
+MAX_AMBIGUITIES_LENGTH = 10 - 1 - 2  # MAX_AMBIG_SIZE (see below) - len(mandatory flag) - len(spaces inbetween)
+
+
 class AmbiguityProperties:
     def __init__(self, mandatory: bool):
         self.mandatory = mandatory
@@ -34,6 +37,7 @@ def dump_ambiguity_properties(ap: dict, fn: str):
 def is_mandatory(err: str, words: list) -> bool:
     """
     This function checks whether a word containing the error string exists.
+
     :param err: error string
     :param words: list of words
     :return: whether the correction can be specified as mandatory
@@ -43,3 +47,36 @@ def is_mandatory(err: str, words: list) -> bool:
             return False
     else:
         return True
+
+
+def extract_ambiguities(ref: str, hyp: str) -> list:
+    """
+    This function extracts identifiable ambiguities. Only ambiguities within the equal amount of words
+    in both reference and hypothesis are processed. Then the code tries to find boundaries of error string,
+    as it subtracts common prefix and suffix of words. If length of both error and correction is > 7, it will not
+    be loaded by Tesseract OCR (search for MAX_AMBIG_SIZE constant in Tesseract source code).
+
+    :param ref: reference string
+    :param hyp: hypothesis string
+    :return: list of tuples, where each tuple contains error and correction
+    """
+    ref_words = ref.split()
+    hyp_words = hyp.split()
+    ambiguities = []
+    if len(ref_words) == len(hyp_words):  # Equal amount of words means ambiguity(-ies) is within one word
+        for rw, hw in zip(ref_words, hyp_words):
+            if rw != hw:
+                error = hw
+                correction = rw
+                # Remove common prefix
+                while len(error) > 1 and len(correction) > 1 and error[0] == correction[0]:
+                    error = error[1:]
+                    correction = correction[1:]
+                # Remove common suffix
+                while len(error) > 1 and len(correction) > 1 and error[-1] == correction[-1]:
+                    error = error[:-1]
+                    correction = correction[:-1]
+                # Store results
+                if len(error) + len(correction) <= MAX_AMBIGUITIES_LENGTH:
+                    ambiguities.append((error, correction))
+    return ambiguities
