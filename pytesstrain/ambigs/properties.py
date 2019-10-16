897832a500dac51b1321e647363ceb39808507c5
Wincent Balin <wincent.balin@gmail.com>
2019-10-16 02:42:28 +0200

    Added ambiguities functions

--- pytesstrain/ambigs/properties.py
+++ pytesstrain/ambigs/properties.py
@@ -24,3 +24,22 @@ class AmbiguityPropertiesJSONEncoder(json.JSONEncoder):
         else:
             return json.JSONEncoder.default(o)
 
+
+def dump_ambiguity_properties(ap: dict, fn: str):
+    with open(fn, 'w', encoding='utf-8') as f:
+        json.dump(sorted(ap.items(), key=lambda i: i[1].count, reverse=True), f,
+                  ensure_ascii=False, indent=4, cls=AmbiguityPropertiesJSONEncoder)
+
+
+def is_mandatory(err: str, words: list) -> bool:
+    """
+    This function checks whether a word containing the error string exists.
+    :param err: error string
+    :param words: list of words
+    :return: whether the correction can be specified as mandatory
+    """
+    for word in words:
+        if err in word:
+            return False
+    else:
+        return True
