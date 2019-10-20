babc2e63001587f9c04a4d1d2a6b69950d3af2e7
Wincent Balin <wincent.balin@gmail.com>
2019-10-20 22:30:17 +0200

    Renamed WER checking utility

--- pytesstrain/cli/language_wer.py
+++ pytesstrain/cli/language_wer.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""Run tests for specified language"""
+"""Run WER tests for specified language"""
 
 import sys
 import argparse
 
+from ..utils import create_word_sequence
+
 
 def main():
     parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
+    parser.add_argument('-l', '--language', help='Language to test')
+    parser.add_argument('-w', '--wordlist', help='Wordlist file')
+    parser.add_argument('-i', '--iterations', type=int, help='Iterations to run', default=100)
     parser.add_argument('-d', '--fonts_dir', help='Directory with fonts')
     parser.add_argument('-f', '--fonts', help='Fonts separated by comma')
     parser.add_argument('-e', '--exposures', help='Exposures separated by comma')
@@ -14,6 +19,7 @@ def main():
 
     fonts = args.fonts.split(',')
     exposures = map(int, args.exposures.split(','))
+    ref = create_word_sequence()
 
 
 if __name__ == '__main__':
