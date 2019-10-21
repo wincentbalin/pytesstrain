9a55b50f2884832e93e2f1f09b14e00d55ba1847
Wincent Balin <wincent.balin@gmail.com>
2019-10-21 13:28:05 +0200

    Refactored function setup_tesseract_path
    Now the function does not try to search for Tesseract in the default path first, as it would prevent using a separate package of Tesseract, in case the system-wide one is installed.

--- pytesstrain/utils/setuppath.py
+++ pytesstrain/utils/setuppath.py
@@ -7,16 +7,14 @@ import pytesseract
 
 def setup_tesseract_path(path: str):
     """
-    Set the supplied path, if tesseract is not found in the default path.
+    Set the supplied path and check for Tesseract binary.
     We assume that all binaries also reside there.
     """
     text2image_cmd = pytesstrain.pytext2image.text2image_cmd
     tesseract_cmd = pytesseract.pytesseract.tesseract_cmd
-    tesseract_found_in_default_path = shutil.which(tesseract_cmd) is not None
-    if not tesseract_found_in_default_path:
-        tesseract_found_in_supplied_path = shutil.which(tesseract_cmd, path=path)
-        if tesseract_found_in_supplied_path:
-            pytesstrain.pytext2image.text2image_cmd = path + os.sep + text2image_cmd
-            pytesseract.pytesseract.tesseract_cmd = path + os.sep + tesseract_cmd
-        else:
-            raise FileNotFoundError('Could not find tesseract cmd either in default path or in supplied path ' + path)
+    tesseract_found_in_supplied_path = shutil.which(tesseract_cmd, path=path)
+    if tesseract_found_in_supplied_path:
+        pytesstrain.pytext2image.text2image_cmd = path + os.sep + text2image_cmd
+        pytesseract.pytesseract.tesseract_cmd = path + os.sep + tesseract_cmd
+    else:
+        raise FileNotFoundError('Could not find tesseract cmd in supplied path ' + path)
