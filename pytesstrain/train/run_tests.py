5e50560c82493e9a41da1344cce1a0078dc08a70
Wincent Balin <wincent.balin@gmail.com>
2019-10-21 17:26:05 +0200

    Implemented function run_tests

--- pytesstrain/train/run_tests.py
+++ pytesstrain/train/run_tests.py
@@ -2,9 +2,17 @@
 
 from itertools import product
 from concurrent.futures import ProcessPoolExecutor
+from typing import List
 
 from .run_test import run_test
 
 
-def run_tests(lang: str, ref: str, wrap: int, fonts_dir: str, fonts: list, exposures: list, config=''):
-    pass
+def run_tests(lang: str, ref: str, wrap: int, fonts_dir: str, fonts: list, exposures: list, config='') -> List[tuple]:
+    """
+    Run run_test function on multiple processors simultaneously.
+    Returns tuples (reference, hypothesis, font, exposure).
+    """
+    with ProcessPoolExecutor(max_workers=None) as executor:
+        futures = [executor.submit(run_test, lang, ref, wrap, fonts_dir, font, exposure, config)
+                   for font, exposure in product(fonts, exposures)]
+    return [future.result() for future in futures]
