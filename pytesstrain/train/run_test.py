77b1ab6fe525f83d3e56cc85e344b3e4e42facb3
Wincent Balin <wincent.balin@gmail.com>
2019-10-21 16:06:36 +0200

    Bugfix
    At least, now we correctly interpret the extension parameter in the run_tesseract function. Also, added config parameter,  which carries command line options to Tesseract executable.

--- pytesstrain/train/run_test.py
+++ pytesstrain/train/run_test.py
@@ -9,11 +9,11 @@ from ..tesseract import run_and_get_output as run_tesseract
 from ..text2image import run_and_get_output as run_text2image
 
 
-def run_test(lang: str, ref: str, wrap: int, fonts_dir: str, font: str, exposure: int):
+def run_test(lang: str, ref: str, wrap: int, fonts_dir: str, font: str, exposure: int, config=''):
     # Create test image
     basefn, txtfn, imgfn, boxfn = run_text2image(textwrap.wrap(ref, wrap), fonts_dir, font, exposure)
     os.remove(txtfn)
     os.remove(boxfn)
     # OCR test image
-    hyp = run_tesseract(imgfn, 'TIF', lang)
+    hyp = run_tesseract(imgfn, 'txt', lang, config=config)
     return ref, hyp, font, exposure
