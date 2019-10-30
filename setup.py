dbf9c6e4ae2c862386d2392a338131587d27ab0b
Wincent Balin <wincent.balin@gmail.com>
2019-10-30 22:19:44 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pytesstrain',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     url='https://github.com/wincentbalin/pytesstrain',
     license='Apache License (2.0)',
