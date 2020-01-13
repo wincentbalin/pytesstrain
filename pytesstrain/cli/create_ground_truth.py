983ce7c9c82620ddb4c1c5ae58d5025118ab62cd
Wincent Balin <wincent.balin@gmail.com>
2020-01-13 22:33:49 +0100

    Bugfixes and refactoring
    Fixed bugs and refactored create_ground_truth tool. Also removed unneeded modules.

--- pytesstrain/cli/create_ground_truth.py
+++ pytesstrain/cli/create_ground_truth.py
@@ -5,19 +5,17 @@ Create single-line ground truth files from source file or directory.
 
 import re
 import sys
-import logging
 import argparse
 from pathlib import Path
-from typing import List, Any
 
 
 RE_LINE_ENDING = re.compile('\r?\n')
 
 
-def generate_gt_txt(source: Path, gt_dir: Path) -> List[str]:
+def generate_gt_txt(source: Path, gt_dir: Path):
     for line_number, line in enumerate(RE_LINE_ENDING.split(source.read_text(encoding='utf8')), 1):
-        gt_txt = gt_dir / '{}.{:06d}.gt.txt'.format(source.name, line_number)
-        gt_txt.write_text(line + '\n', encoding='utf-8')
+        gt_txt = gt_dir / '{}.{:06d}.gt.txt'.format(source.stem, line_number)
+        gt_txt.write_text(line.rstrip() + '\n', encoding='utf-8')
 
 
 def main():
@@ -26,17 +24,14 @@ def main():
     parser.add_argument('gt_dir', help='Ground truth directory')
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
-
-    gt_txt_files = []  # type: List[str]
     source = Path(args.source)
-    gt_dir = Path(args.gtdir)
+    gt_dir = Path(args.gt_dir)
 
     if source.is_dir():
         for path in source.rglob('*.txt'):
-            gt_txt_files += generate_gt_txt(path, gt_dir)
+            generate_gt_txt(path, gt_dir)
     else:
-        gt_txt_files += generate_gt_txt(source, gt_dir)
+        generate_gt_txt(source, gt_dir)
 
 
 if __name__ == '__main__':
