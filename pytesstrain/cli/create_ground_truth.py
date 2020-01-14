a0aa9a532db5761e86957b6c1a1161c566c593e0
Wincent Balin <wincent.balin@gmail.com>
2020-01-14 07:40:45 +0100

    Collect .gt.txt paths

--- pytesstrain/cli/create_ground_truth.py
+++ pytesstrain/cli/create_ground_truth.py
@@ -7,18 +7,21 @@ import re
 import sys
 import argparse
 from pathlib import Path
-
+from typing import List
 
 RE_LINE_ENDING = re.compile('\r?\n')
 
 
-def generate_gt_txt(source: Path, gt_dir: Path):
+def generate_gt_txt(source: Path, gt_dir: Path) -> List[Path]:
+    gt_txt = []
     for line_number, line in enumerate(RE_LINE_ENDING.split(source.read_text(encoding='utf8')), 1):
         tidy_line = line.rstrip()
         if not tidy_line:  # Get rid of empty lines
             continue
         gt_txt = gt_dir / '{}.{:06d}.gt.txt'.format(source.stem, line_number)
         gt_txt.write_text(tidy_line + '\n', encoding='utf-8')
+        gt_txt.append(gt_txt)
+    return gt_txt
 
 
 def main():
@@ -30,11 +33,12 @@ def main():
     source = Path(args.source)
     gt_dir = Path(args.gt_dir)
 
+    gt_txt = []
     if source.is_dir():
         for path in source.rglob('*.txt'):
-            generate_gt_txt(path, gt_dir)
+            gt_txt += generate_gt_txt(path, gt_dir)
     else:
-        generate_gt_txt(source, gt_dir)
+        gt_txt += generate_gt_txt(source, gt_dir)
 
 
 if __name__ == '__main__':
